e7fab7fb80352e7fa99033dfef059088119c45d5
gustavoferrarigranero <gustavoferrarigranero@github.com>
2014-08-15 17:04:14 -0300

    classes daos prontas

--- gen/net/placeforme/R.java
+++ gen/net/placeforme/R.java
@@ -35,10 +35,11 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_tab_one=0x7f030001;
-        public static final int fragment_tab_tree=0x7f030002;
-        public static final int fragment_tab_two=0x7f030003;
-        public static final int list_eventos=0x7f030004;
+        public static final int background_actionbar=0x7f030001;
+        public static final int fragment_tab_one=0x7f030002;
+        public static final int fragment_tab_tree=0x7f030003;
+        public static final int fragment_tab_two=0x7f030004;
+        public static final int list_eventos=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/net/placeforme/MainActivity.java
+++ src/net/placeforme/MainActivity.java
@@ -43,10 +43,12 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        
         // Set up the action bar.
         final ActionBar actionBar = this.getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
